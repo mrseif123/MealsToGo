0d573595264e1953137877b58f3df55df5590932
Seaf Aliyan <codo47@gmail.com>
2022-08-18 16:37:11 +0300

    added searchbar

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Searchbar } from "react-native-paper";
+import { Provider as PaperProvider } from "react-native-paper";
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = React.useState("");
+  const onChangeSearch = (query) => {
+    setSearchQuery(query);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <PaperProvider>
+        <View style={styles.container}>
+          <View style={styles.searchContainer}>
+            <Searchbar
+              placeholder="Search"
+              onChange={onChangeSearch}
+              value={searchQuery}
+            />
+          </View>
+          <View style={styles.listContainer}>
+            <Text style={styles.list}>list</Text>
+          </View>
+        </View>
+        <ExpoStatusBar style="auto" />
+      </PaperProvider>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
+  },
+
+  searchContainer: {
+    backgroundColor: "white",
+    flex: 0.05,
+    width: "100%",
+    padding: 10,
+    justifyContent: "center",
+    alignContent: "flex-start",
+  },
+  searchText: {},
+
+  listContainer: {
+    backgroundColor: "yellow",
+    padding: 10,
+    flex: 1,
+    width: "100%",
+    height: "100%",
   },
 });
