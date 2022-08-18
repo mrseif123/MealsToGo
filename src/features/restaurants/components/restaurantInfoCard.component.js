5fa38043e8c4c77d9afcd8e0fe99c97b186c29be
Seaf Aliyan <codo47@gmail.com>
2022-08-18 19:23:36 +0300

    Restaurant card

--- src/features/restaurants/components/restaurantInfoCard.component.js
+++ src/features/restaurants/components/restaurantInfoCard.component.js
@@ -1,32 +1,83 @@
 import React from "react";
 import { Card } from "react-native-paper";
 import styled from "styled-components";
-
-const RestaurantCard = styled(Card)``;
+import { Image, View, Text } from "react-native";
+const RestaurantCard = styled(Card)`
+  height: 300px;
+`;
 
 const RestaurantCardCover = styled(Card.Cover)`
   background-color: ${(props) => props.theme.colors.ui.tertiary};
 `;
 
 const Title = styled.Text`
+  font-size: ${(props) => props.theme.fontSizes.title};
+`;
+
+const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.ui.tertiary};
+`;
+
+const Address = styled.Text`
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[1]};
+  padding-bottom: ${(props) => props.theme.space[1]};
+`;
+
+const RatingAndOpenNowView = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const RestaurantInfoIcon = styled(Image)`
+  width: 20px;
+  height: 20px;
 `;
 
 export const RestaurantInfo = ({ restaurant = {} }) => {
   const {
     name = "restaurant name",
-    icon = "https://via.placeholder.com/150",
+    icon = "https://picsum.photos/700",
     photos = ["https://picsum.photos/700", "https://via.placeholder.com/150"],
     address = "restaurant address",
     isOpenNow = true,
-    rating = 4,
-    isClosedTemporarily = false,
+    rating = 5,
+    isClosedTemporarily = true,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <RatingAndOpenNowView>
+          <Rating>
+            {ratingArray.map((key) => (
+              <RestaurantInfoIcon
+                key={key}
+                source={require("../../../../assets/rating.jpg")}
+              />
+            ))}
+          </Rating>
+          {isClosedTemporarily && (
+            <Text style={{ color: "red" }}>Closed temporarily</Text>
+          )}
+          {isOpenNow && (
+            <RestaurantInfoIcon
+              source={require("../../../../assets/open.jpg")}
+            />
+          )}
+          <RestaurantInfoIcon source={require("../../../../assets/bed.png")} />
+        </RatingAndOpenNowView>
+
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
