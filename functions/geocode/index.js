7342b55499243383c6addfc8e218fdde382c4c8a
Seaf Aliyan <codo47@gmail.com>
2022-08-23 15:54:37 +0300

    added FB connection to local host mockups

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,8 +1,9 @@
 const { locations: locationsMock } = require("./geocode.mock");
 const url = require("url");
 
-module.exports.geocodeRequest = function (request, response) {
+module.exports.geocodeRequest = (request, response) => {
   const { city } = url.parse(request.url, true).query;
   const locationMock = locationsMock[city.toLowerCase()];
-  response.send(locationMock);
+
+  response.json(locationMock);
 };
